e718455da6bde569573bd7d260d669ca7485984c
dours <akadours2@gmail.com>
2020-05-21 00:18:29 +0300

    finally works

--- testbulk.cpp
+++ testbulk.cpp
@@ -1,18 +1,24 @@
 
-#include <libusb.h>
+#include <libusb-1.0/libusb.h>
+#include <cstdio>
+#include <unistd.h>
+#include <cassert>
 
 int main() { 
-
-  libusb_device** list;
-  assert(0 == libusb_get_device_list(&context, &list));
-
-
-  libusb_device_handle* handle; 
-  assert(0 == libusb_open(device, &handle)); 
-
-  while (1) { 
+  libusb_context* context;
+  libusb_init(&context); 
+  libusb_device_handle* handle = libusb_open_device_with_vid_pid(context, 0x2047, 0x0301);
+  assert(handle); 
+  assert(0 == libusb_claim_interface(handle, 0));
+  for (int iteration = 0; 1; ++iteration) { 
     int transferred = -1; 
-    int error = libusb_bulk_transfer(handle, 0x81, buf, 64, &transferred, 10); 
-    assert(64 == transferred);
+    unsigned char buf[64]; 
+    int error = libusb_bulk_transfer(handle, 0x81, buf, 64, &transferred, 100); 
+    if (0 != error) fprintf(stderr, "error = %i\n", error); 
     assert(0 == error); 
+    assert(64 == transferred);
+    fprintf(stderr, "N%i %ibytes buf[0] = %02x, buf[1] = %02x\n", iteration, transferred, buf[0], buf[1]); 
+    usleep(1000); 
+  }
+}
 
