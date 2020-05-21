1cb7fef80af312bab8473799354c88e2a81f96ba
dours <akadours2@gmail.com>
2020-05-21 21:52:29 +0300

    support memory comands on the C++ level, implement a test (compilable, not tested)

--- testbulk.cpp
+++ testbulk.cpp
@@ -3,8 +3,55 @@
 #include <cstdio>
 #include <unistd.h>
 #include <cassert>
+#include <stdint.h>
+#include <vector>
+#include <cstring>
+using namespace std; 
+
+extern "C" {
+#include "memoryCommand.h"
+#include "msp430f5510_symbols.h"
+}
+
+class MemoryCommands {
+  int size; 
+  uint16_t* data;
+
+public: 
+  MemoryCommands(vector<tmemoryCommand>& v)  {
+    static_assert(sizeof(tmemoryCommand) % sizeof(uint16_t) == 0); 
+    size = sizeof(uint16_t) + v.size() * sizeof(tmemoryCommand); 
+    data = new uint16_t[size / sizeof(uint16_t)]; 
+    data[0] = HEADER_MAGIC;
+    memcpy(data + 1, v.data(), v.size() * sizeof(tmemoryCommand)); 
+  }
+  ~MemoryCommands() { delete[] data; } 
+  unsigned char* getBuf() const { return reinterpret_cast<unsigned char*>(data); } 
+  int getSize() const { return size; } 
+};
+
+tmemoryCommand mkSetBits(uint16_t dst, uint16_t value) { 
+  tmemoryCommand c { SetBits, dst, value }; 
+  return c; 
+}
+
+tmemoryCommand mkClearBits(uint16_t dst, uint16_t value) { 
+  tmemoryCommand c { ClearBits, dst, value }; 
+  return c; 
+}
+
+tmemoryCommand mkAssign(uint16_t dst, uint16_t value) { 
+  tmemoryCommand c { Assign, dst, value }; 
+  return c; 
+}
 
 int main() { 
+  vector<tmemoryCommand> v { mkSetBits(PAOUT, 1 << 4) };
+  MemoryCommands set1(v);
+  vector<tmemoryCommand> u { mkClearBits(PAOUT, 1 << 4) } ;
+  MemoryCommands clr1(u); 
+
+
   libusb_context* context;
   libusb_init(&context); 
   libusb_device_handle* handle = libusb_open_device_with_vid_pid(context, 0x2047, 0x0301);
@@ -26,6 +73,20 @@ int main() {
     if (iteration == 0) iteration = value; 
     assert(value == (iteration & 0xffff)); 
 #endif
+  
+    error = libusb_bulk_transfer(handle, 0x01, set1.getBuf(), set1.getSize(), &transferred, 100); 
+    if (0 != error) fprintf(stderr, "error = %i\n", error); 
+    assert(0 == error); 
+    assert(64 == transferred);
+    usleep(500); 
+
+    error = libusb_bulk_transfer(handle, 0x01, clr1.getBuf(), clr1.getSize(), &transferred, 100); 
+    if (0 != error) fprintf(stderr, "error = %i\n", error); 
+    assert(0 == error); 
+    assert(64 == transferred);
+    usleep(500); 
+
+
   }
 }
 
